7b3e0fbe7bf120ee24c5ee3f3345edcc3e906ec0
1sKaNdRR <151454522+1sKaNdRR@users.noreply.github.com>
2024-08-25 20:59:18 +0200

    node-hw1-20

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,6 +1,2 @@
 import * as path from "node:path";
-export const PATH_DB_CONTACTS = path.resolve("src", "db", "db-contacts.json");
-
-
-
-// export const PATH_DB_SONGS = path.join(process.cwd(), "src", "db", "db-songs.json");;
+export const PATH_DB_CONTACTS = path.resolve("src/db/db.json");
